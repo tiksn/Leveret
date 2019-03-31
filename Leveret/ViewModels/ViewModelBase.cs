afba73883c376b9d3c5b1efffe3eb45135c9debb
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-03-31 15:38:58 +0300

    initial project

---
+++ Leveret/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Leveret.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
