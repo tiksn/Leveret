79b300ff584a1739d3a68059660e847c0efdee6f
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-04-01 01:11:37 +0300

    input code

--- Leveret/ViewModels/MainWindowViewModel.cs
+++ Leveret/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ReactiveUI;
+using System;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TIKSN.Leveret.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        private readonly ObservableAsPropertyHelper<string> _executionResults;
+        private string _inputSourceCode;
+
+        public MainWindowViewModel()
+        {
+            _executionResults = this
+            .WhenAnyValue(x => x.InputSourceCode)
+            .Throttle(TimeSpan.FromMilliseconds(800))
+            .DistinctUntilChanged()
+            .Where(code => !string.IsNullOrWhiteSpace(code))
+            .SelectMany(ExecuteCodeAsync)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .ToProperty(this, x => x.ExecutionResults);
+        }
+
+        public string ExecutionResults => _executionResults.Value;
+
+        public string InputSourceCode
+        {
+            get => _inputSourceCode;
+            set => this.RaiseAndSetIfChanged(ref _inputSourceCode, value);
+        }
+
+        private async Task<string> ExecuteCodeAsync(string code, CancellationToken token)
+        {
+            Console.WriteLine(code);
+            return code;
+        }
     }
 }
