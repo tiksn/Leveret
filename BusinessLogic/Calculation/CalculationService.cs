6b26dac7d061e3544bff520bba55fd3fd02d3a6b
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-04-08 00:11:00 +0300

    business logic added

---
+++ BusinessLogic/Calculation/CalculationService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Leveret.BusinessLogic.Calculation
+{
+    public class CalculationService : ICalculationService
+    {
+        private readonly ILogger<CalculationService> _logger;
+
+        public CalculationService(ILogger<CalculationService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<string> CalculateAsync(string sourceCode, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation(sourceCode);
+            return sourceCode;
+        }
+    }
+}
---
+++ BusinessLogic/Calculation/ICalculationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Leveret.BusinessLogic.Calculation
+{
+    public interface ICalculationService
+    {
+        Task<string> CalculateAsync(string sourceCode, CancellationToken cancellationToken);
+    }
+}
