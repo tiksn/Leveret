369fc530afd7f849c7e7ef38d61cfa10e24015c9
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-04-20 13:31:07 +0300

    linux and rhel build added

--- psake.ps1
+++ psake.ps1
@@ -1,4 +1,4 @@
-Task Build -Depends BuildWin7x64,BuildWin7x86
+Task Build -Depends BuildWin7x64,BuildWin7x86,BuildLinux64,BuildRhel64
 
 Task BuildWin7x64 -Depends PreBuild {
    $script:publishWin7x64Folder = Join-Path -Path $script:publishFolder -ChildPath "win7x64"
@@ -10,7 +10,19 @@ Task BuildWin7x86 -Depends PreBuild {
     $script:publishWin7x86Folder = Join-Path -Path $script:publishFolder -ChildPath "win7x86"
  
     Exec { dotnet publish ".\Leveret\Leveret.csproj" --output $script:publishWin7x86Folder --self-contained --runtime win7-x86 }
- }
+}
+
+Task BuildLinux64 -Depends PreBuild {
+    $script:publishLinux64Folder = Join-Path -Path $script:publishFolder -ChildPath "linux64"
+ 
+    Exec { dotnet publish ".\Leveret\Leveret.csproj" --output $script:publishLinux64Folder --self-contained --runtime linux-x64 }
+}
+
+Task BuildRhel64 -Depends PreBuild {
+    $script:publishRhel64Folder = Join-Path -Path $script:publishFolder -ChildPath "rhel64"
+ 
+    Exec { dotnet publish ".\Leveret\Leveret.csproj" --output $script:publishRhel64Folder --self-contained --runtime rhel-x64 }
+}
 
 Task PreBuild -Depends Init,Clean {
     $script:publishFolder = Join-Path -Path $script:trashFolder -ChildPath "publish"
