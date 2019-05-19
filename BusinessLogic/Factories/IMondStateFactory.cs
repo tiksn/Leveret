9b1bf375ab5c98f80c6cdd9eea1076057ad917a9
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-05-20 00:13:53 +0300

    MondStateFactory created

---
+++ BusinessLogic/Factories/IMondStateFactory.cs
@@ -0,0 +1,9 @@
+﻿using Mond;
+
+namespace TIKSN.Leveret.BusinessLogic.Factories
+{
+    public interface IMondStateFactory
+    {
+        MondState Create();
+    }
+}
---
+++ BusinessLogic/Factories/MondStateFactory.cs
@@ -0,0 +1,21 @@
+﻿using Mond;
+
+namespace TIKSN.Leveret.BusinessLogic.Factories
+{
+    public class MondStateFactory : IMondStateFactory
+    {
+        public MondState Create()
+        {
+            var state = new MondState()
+            {
+                Options = new MondCompilerOptions()
+                {
+                    MakeRootDeclarationsGlobal = true,
+                    UseImplicitGlobals = true
+                }
+            };
+
+            return state;
+        }
+    }
+}
