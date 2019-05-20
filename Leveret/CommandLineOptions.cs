1239c8058f853aa36ccba73014d762182414c587
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-05-20 12:28:17 +0300

    options added

---
+++ Leveret/CommandLineOptions.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace TIKSN.Leveret
+{
+    public class CommandLineOptions
+    {
+        [Option('f', "file", Required = false, HelpText = "File to open.")]
+        public string File { get; set; }
+    }
+}
