f1aacae4f5ffe98a9cb1e35df17b272a92ce9d1f
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-05-16 14:53:43 +0200

    Update ViewModelBase.cs

--- Leveret/ViewModels/ViewModelBase.cs
+++ Leveret/ViewModels/ViewModelBase.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ReactiveUI;
+﻿using ReactiveUI;
 
-namespace TIKSN.Leveret.ViewModels
+namespace TIKSN.Leveret.ViewModels;
+
+public class ViewModelBase : ReactiveObject
 {
-    public class ViewModelBase : ReactiveObject
-    {
-    }
 }
