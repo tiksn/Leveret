2f28fc09667b7be5b9f58544837158f8031398ce
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-03-31 21:00:14 +0200

    Update chocolateyInstall.ps1

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $zipFile = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Getting x64 bit zip"; Get-Item "$toolsDir\win7x64.zip"
-} else { Write-Host "Getting x32 bit zip"; Get-Item "$toolsDir\win7x86.zip" }
+         Write-Host "Getting x64 bit zip"; Get-Item "$toolsDir\winx64.zip"
+} else { Write-Host "Getting x32 bit zip"; Get-Item "$toolsDir\winx86.zip" }
 
 Get-ChocolateyUnzip -FileFullPath $zipfile -Destination $toolsDir
 
